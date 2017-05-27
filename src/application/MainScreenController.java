656a7e6c9db662e7e83ea126ce3f11957bee1dda
Nayib <nayibmc@gmail.com>
2017-05-27 05:14:12 -0600

    caca

--- src/application/MainScreenController.java
+++ src/application/MainScreenController.java
@@ -1,19 +1,28 @@
 package application;
 
+import HTTP_Handling.DTO.Appointment;
+import HTTP_Handling.DTO.ClinicalCase;
+import datastructures.binarytree.BinaryNode;
+import datastructures.binarytree.BinaryTree;
+import datastructures.linkedlist.LinkedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 public class MainScreenController {
     public static String IP;
     @FXML
-    private TreeView<?> treeView;
+    private TreeView<ClinicalCase> treeView;
 
     @FXML
     private Label bottom_label;
 
     @FXML
-    private ListView<?> schedule_area;
+    private ListView<String> schedule_area;
+
+    @FXML
+    private VBox layout;
 
     @FXML
     private Button update_button;
@@ -26,11 +35,23 @@ public class MainScreenController {
 
     @FXML
     private Button sendButtton;
+
+    @FXML
+    private Button update_cc;
+
     @FXML
     void updateSchedule(ActionEvent event) {
-
+        //LinkedList<Appointment> ListaCitasRecibida = ;
+        schedule_area.getItems().addAll("Hola","Ja");
     }
 
+    @FXML
+    void updateCases(ActionEvent event){
+        //BinaryTree casos = ;
+
+        //for
+        TreeItem<String> root;
+    }
 
     public TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
         TreeItem<String> item = new TreeItem<>(title);
